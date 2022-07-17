1fa25df4a16bf5d2edb45eec69703be4bc3c68a6
saikat111 <s8856010@gmail.com>
2022-07-17 16:52:48 +0500

    show all data

--- app/src/main/java/com/code/todolist/DataAddActivity.java
+++ app/src/main/java/com/code/todolist/DataAddActivity.java
@@ -8,22 +8,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.code.todolist.databinding.ActivityDataAddBinding;
+import com.code.todolist.databinding.ActivityMainBinding;
+
 public class DataAddActivity extends AppCompatActivity {
-    EditText title,text;
-    Button submit;
+    ActivityDataAddBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_add);
-        title = findViewById(R.id.t);
-        text = findViewById(R.id.te);
-        submit = findViewById(R.id.submit);
-        submit.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityDataAddBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                intent.putExtra("title", title.getText());
-                intent.putExtra("text", text.getText());
+                intent.putExtra("title",binding.t.getText().toString());
+                intent.putExtra("text", binding.te.getText().toString());
                 setResult(RESULT_OK,intent);
                 finish();
             }
--- app/src/main/java/com/code/todolist/MainActivity.java
+++ app/src/main/java/com/code/todolist/MainActivity.java
@@ -2,30 +2,45 @@ package com.code.todolist;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.code.todolist.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton floatingActionButton;
+    ActivityMainBinding binding;
     private NoteViewModel noteViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         noteViewModel = new ViewModelProvider(this, (ViewModelProvider.Factory) ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication()))
                 .get(NoteViewModel.class);
-        floatingActionButton = findViewById(R.id.floatingActionButton);
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        binding.floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,DataAddActivity.class);
                 startActivityForResult(intent,1);
+            }
+        });
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setHasFixedSize(true);
+        RVAdapter adapter = new RVAdapter();
+        binding.recyclerView.setAdapter(adapter);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                adapter.submitList(notes);
             }
         });
     }
